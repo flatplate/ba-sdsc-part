fe9f9625b04b1528bfe020aae73ff990a896bb52
Ural Bayhan <uralbayhan0@gmail.com>
2021-05-22 03:24:02 +0200

    Add missing question types to app

--- app/src/components/question/Question.jsx
+++ app/src/components/question/Question.jsx
@@ -1,10 +1,10 @@
-import React from "react";
-import SingleAnswerMultipleChoiceQuestion from "./SingleAnswerMultipleChoiceQuestion";
-import MultipleAnswerQuestion from "./MultipleAnswerQuestion";
-import OrderedMultipleAnswerQuestion from "./OrderedMultipleAnswerQuestion";
-import MatrixAnswerQuestion from "./MatrixAnswerQuestion";
-import DataQuestion from "./DataQuestion";
-import ContactForm from "../ContactForm";
+import React from 'react';
+import SingleAnswerMultipleChoiceQuestion from './SingleAnswerMultipleChoiceQuestion';
+import MultipleAnswerQuestion from './MultipleAnswerQuestion';
+import OrderedMultipleAnswerQuestion from './OrderedMultipleAnswerQuestion';
+import MatrixAnswerQuestion from './MatrixAnswerQuestion';
+import DataQuestion from './DataQuestion';
+import ContactForm from '../ContactForm';
 
 class Question extends React.Component {
     constructor(props, context) {
@@ -14,26 +14,26 @@ class Question extends React.Component {
     render() {
         console.log(this.props.question.type);
         switch (this.props.question.type) {
-            case "Single Answer Question":
+            case 'Single Answer Question':
                 return <SingleAnswerMultipleChoiceQuestion {...this.props} />;
-            case "Single Answer Question With Other":
-                return (
-                    <SingleAnswerMultipleChoiceQuestion {...this.props} other />
-                );
-            case "Multiple Answer Question":
+            case 'Single Answer Question With Other':
+                return <SingleAnswerMultipleChoiceQuestion {...this.props} other />;
+            case 'Multiple Answer Question':
                 return <MultipleAnswerQuestion {...this.props} />;
-            case "Multiple Answer Ordered Question With Other":
+            case 'Multiple Answer Question With Other':
+                return <MultipleAnswerQuestion {...this.props} other />;
+            case 'Multiple Answer Ordered Question':
+                return <OrderedMultipleAnswerQuestion {...this.props} />;
+            case 'Multiple Answer Ordered Question With Other':
                 return <OrderedMultipleAnswerQuestion {...this.props} other />;
-            case "Matrix Question":
-                return <MatrixAnswerQuestion {...this.props}/>;
-            case "Data Question":
-                return <DataQuestion {...this.props}/>;
-            case "Contact Form Question":
-                return <ContactForm {...this.props}/>;
+            case 'Matrix Question':
+                return <MatrixAnswerQuestion {...this.props} />;
+            case 'Data Question':
+                return <DataQuestion {...this.props} />;
+            case 'Contact Form Question':
+                return <ContactForm {...this.props} />;
             default:
-                return (
-                    <pre>{JSON.stringify(this.props.question, null, 2)}</pre>
-                );
+                return <pre>{JSON.stringify(this.props.question, null, 2)}</pre>;
         }
         return null;
     }
